1015559e8e9eb3bb5276390e5406bbb46c8e9b2c
dcnb <devinbecker@gmail.com>
2022-04-22 13:10:30 -0700

    constants set onload?

--- _includes/js/dd-items.js
+++ _includes/js/dd-items.js
@@ -1,6 +1,8 @@
 /* Load items from Sheets, store in sessionStorage, or load from sessionStorage */
 var dd_items = [];
 var dd_scenes = [];
+var scenesNav = [];
+
 if (sessionStorage.getItem("dd_metadata_set")){
   var current_metadata = sessionStorage.getItem("dd_metadata_set");}
 
@@ -13,6 +15,7 @@ function dd_items_init(results) {
   sessionStorage.setItem("dd_title", dd_items[1].play);
   console.log(dd_items[1].play);
   pageInit(dd_items,dd_scenes);
+  initial_scenes();
 }
 
 // check for sessionStored items
@@ -47,3 +50,18 @@ function reset_dd_items(){
   location.reload(); 
 };
 
+//for initial loads
+
+
+
+function initial_scenes(){
+  for (var i = 0, len = dd_scenes.length; i < len; i++) {
+    var ref_url = "{{ '/?scene=' | relative_url }}";
+      var act = dd_scenes[i].split('.')[0];
+      var scene = dd_scenes[i].split('.')[1];
+      console.log(dd_scenes[i] + "-- Act" + act + " scene " + scene );
+      var sceneClass = 'act' + act + 'scene' + scene;
+      scenesNav += '<li class="dropdown-item"><a class="link-dark rounded " href="'+ ref_url +'act' + act + 'scene' + scene +'">Act ' + act + ', Scene ' + scene +'</a></li>'
+      
+  } 
+  $("#scenesNavDropdown").html(scenesNav);}
